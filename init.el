651157bff9de3f2af2410f3b0e8d3eab09e04d14
Ben Anhalt <anhalt@ku.edu>
2012-04-27 14:14:37 -0500

    initial commit

---
+++ init.el
@@ -0,0 +1,41 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+
+(let ((default-directory "~/.emacs.d/site-lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(require 'scala-mode-auto)
+
+;; Load the ensime lisp code...
+;;(add-to-list 'load-path "~/ensime/elisp/")
+;;(require 'ensime)
+
+;; This step causes the ensime-mode to be started whenever
+;; scala-mode is started for a buffer. You may have to customize this step
+;; if you're not using the standard scala mode.
+;;(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+;; MINI HOWTO: 
+;; Open .scala file. M-x ensime (once per project)
+
+(setq-default indent-tabs-mode nil)
+(setq-default show-trailing-whitespace t)
+
+(setq frame-title-format
+      '((buffer-file-name "%f" (dired-directory dired-directory "%b")) " - "
+        invocation-name "@" system-name))
+
+(require 'geiser-install)
+(require 'quack)
+(quack-install)
