cfc3e77a03728e803c44972a793dd17ce3fc2290
Ben Anhalt <ben.anhalt@gmail.com>
2012-05-08 21:23:39 -0500

    add csharp-mode

--- init.el
+++ init.el
@@ -52,3 +52,8 @@
 (setq kill-buffer-query-functions
       (remq 'process-kill-buffer-query-function
             kill-buffer-query-functions))
+
+;; csharp mode
+(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
+(setq auto-mode-alist
+      (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
