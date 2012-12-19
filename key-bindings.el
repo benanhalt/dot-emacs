5dcec468c6407d40044b6534a82e8e87deddb490
Ben Anhalt <anhalt@ku.edu>
2012-12-19 15:10:37 -0600

    make key bindings into minor mode so that they override major modes

--- key-bindings.el
+++ key-bindings.el
@@ -1,22 +1,36 @@
 (global-set-key (kbd "C-x C-b") 'ido-switch-buffer)
 
 (global-set-key (kbd "C-a") 'back-to-indentation-or-beginning)
-
-(define-key global-map (kbd "C-'") 'ace-jump-mode)
-
-(global-set-key (kbd "C-=") 'er/expand-region)
-
 (global-set-key (kbd "C-x r t") 'inline-string-rectangle)
-(global-set-key (kbd "C-<") 'mark-previous-like-this)
-(global-set-key (kbd "C->") 'mark-next-like-this)
-(global-set-key (kbd "C-M-m") 'mark-more-like-this)
-(global-set-key (kbd "C-*") 'mark-all-like-this)
-
-(global-set-key (kbd "M-m") 'jump-char-forward)
-(global-set-key (kbd "M-M") 'jump-char-backward)
-
-(global-set-key (kbd "C-w") 'kill-region-or-backward-word)
 
 (global-set-key (kbd "C-x m") 'magit-status)
+
+
+(defvar my-keys-minor-mode-map (make-keymap) "my-keys-minor-mode keymap.")
+
+(define-key my-keys-minor-mode-map (kbd "C-'") 'ace-jump-mode)
+
+(define-key my-keys-minor-mode-map (kbd "C-=") 'er/expand-region)
+
+(define-key my-keys-minor-mode-map (kbd "C-<") 'mark-previous-like-this)
+(define-key my-keys-minor-mode-map (kbd "C->") 'mark-next-like-this)
+(define-key my-keys-minor-mode-map (kbd "C-M-m") 'mark-more-like-this)
+(define-key my-keys-minor-mode-map (kbd "C-*") 'mark-all-like-this)
+
+(define-key my-keys-minor-mode-map (kbd "M-m") 'jump-char-forward)
+(define-key my-keys-minor-mode-map (kbd "M-M") 'jump-char-backward)
+
+(define-key my-keys-minor-mode-map (kbd "C-w") 'kill-region-or-backward-word)
+
+(define-minor-mode my-keys-minor-mode
+  "A minor mode so that my key settings override annoying major modes."
+  t " my-keys" 'my-keys-minor-mode-map)
+
+(my-keys-minor-mode 1)
+
+(defun my-minibuffer-setup-hook ()
+  (my-keys-minor-mode 0))
+
+(add-hook 'minibuffer-setup-hook 'my-minibuffer-setup-hook)
 
 (provide 'key-bindings)
