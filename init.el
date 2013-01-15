166097a8229245202cfe461fe9b6e09a2b70ece1
Ben Anhalt <anhalt@ku.edu>
2013-01-15 15:23:48 -0600

    put backup files to own directory

--- init.el
+++ init.el
@@ -122,3 +122,11 @@
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((python . t)))
+
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+;; Make backups of files even when they're in version control
+(setq vc-make-backup-files t)
