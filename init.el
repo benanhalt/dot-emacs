cd1cb920d37def0c0eb52e36283f3ee925cf0839
Ben Anhalt <anhalt@ku.edu>
2013-05-03 17:57:15 -0500

    clean up start-up

--- init.el
+++ init.el
@@ -1,48 +1,28 @@
+;;; -*- lexical-binding: t -*-
 
-;; Set path to .emacs.d
-(setq dotfiles-dir (file-name-directory
-                    (or (buffer-file-name) load-file-name)))
+;; Go ahead and load cl since ace-jump-mode is broken and depends on it.
+(require 'cl)
 
-;; Keep emacs Custom-settings in separate file
-(setq custom-file (expand-file-name "custom.el" dotfiles-dir))
-(load custom-file)
+(let* ((dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+       (site-lisp-dir (expand-file-name "site-lisp" dotfiles-dir)))
 
-;; Set path to dependencies
-(setq site-lisp-dir (expand-file-name "site-lisp" dotfiles-dir))
+  (add-to-list 'load-path dotfiles-dir)
+  (add-to-list 'load-path site-lisp-dir)
 
-;; Set up load path
-(add-to-list 'load-path dotfiles-dir)
-(add-to-list 'load-path site-lisp-dir)
+  ;; Keep emacs Custom-settings in separate file
+  (setq custom-file (expand-file-name "custom.el" dotfiles-dir))
+  (load custom-file)
 
-;; Add external projects to load path
-(dolist (project (directory-files site-lisp-dir t "\\w+"))
-  (when (file-directory-p project)
-    (add-to-list 'load-path project)))
+  ;; Add external projects to load path
+  (dolist (project (directory-files site-lisp-dir t "\\w+"))
+    (when (file-directory-p project)
+      (add-to-list 'load-path project)))
 
-(byte-recompile-directory (expand-file-name "~/.emacs.d") 0)
-
-(require 'scala-mode-auto)
-
-;; Load the ensime lisp code...
-;;(add-to-list 'load-path "~/ensime/elisp/")
-;;(require 'ensime)
-
-;; This step causes the ensime-mode to be started whenever
-;; scala-mode is started for a buffer. You may have to customize this step
-;; if you're not using the standard scala mode.
-;;(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-
-;; MINI HOWTO: 
-;; Open .scala file. M-x ensime (once per project)
-
-(setq-default indent-tabs-mode nil)
-(setq-default show-trailing-whitespace t)
-
-;; Functions (load all files in defuns-dir)
-(setq defuns-dir (expand-file-name "defuns" dotfiles-dir))
-(dolist (file (directory-files defuns-dir t "\\w+"))
-  (when (file-regular-p file)
-    (load file)))
+  ;; Functions (load all files in defuns-dir)
+  (setq defuns-dir (expand-file-name "defuns" dotfiles-dir))
+  (dolist (file (directory-files defuns-dir t "\\w+"))
+    (when (file-regular-p file)
+      (load file))))
 
 (setq frame-title-format
       '((buffer-file-name "%f" (dired-directory dired-directory "%b")) " - "
@@ -52,11 +32,8 @@
 (setq ido-everywhere t)
 (ido-mode 1)
 
-(when (require 'geiser-install nil t)
-  (setq geiser-repl-startup-time 20000))
-
-(when (require 'quack nil t)
-  (quack-install))
+;; (when (require 'geiser-install nil t)
+;;   (setq geiser-repl-startup-time 20000))
 
 ;; disabling prompts
 (fset 'yes-or-no-p 'y-or-n-p)
@@ -67,52 +44,18 @@
       (remq 'process-kill-buffer-query-function
             kill-buffer-query-functions))
 
-;; csharp mode
-(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
-(setq auto-mode-alist
-      (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
-
-(setq auto-mode-alist
-      (append '(("\\.m$" . octave-mode)) auto-mode-alist))
-
 (require 'setup-package)
-(packages-install
- (cons 'magit melpa)
- (cons 'fill-column-indicator marmalade)
- (cons 'undo-tree marmalade)
- (cons 'visual-regexp melpa)
- (cons 'flymake-cursor marmalade))
 
 (eval-after-load 'magit '(require 'setup-magit))
 (eval-after-load 'flymake '(require 'setup-flymake))
 
-(require 'typing)
-(autoload 'typing-of-emacs "Typing of Emacs" t)
-
-(require 'coffee-mode)
-(require 'ace-jump-mode)
-(require 'jump-char)
-(require 'expand-region)
-(require 'inline-string-rectangle)
-(require 'mark-more-like-this)
-(require 'visual-regexp)
-(require 'fill-column-indicator)
-(require 'undo-tree)
-
-(autoload 'markdown-mode "markdown-mode"
-  "Major mode for editing Markdown files" t)
-(setq auto-mode-alist
-      (cons '("\\.md" . markdown-mode) auto-mode-alist))
-
-
-;; JavaScript
-(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
 
 ;; slime
 (setq inferior-lisp-program "sbcl") ; your Lisp system
-(require 'slime-autoloads)
 (slime-setup)
 
 (require 'key-bindings)
@@ -141,7 +84,12 @@
 (setq auto-revert-verbose nil)
 
 (show-paren-mode 1)
-(global-undo-tree-mode)
 (setq mouse-yank-at-point t)
+
+(setq-default indent-tabs-mode nil)
+(setq-default show-trailing-whitespace t)
+
+(require 'undo-tree)
+(global-undo-tree-mode)
 
 (server-start)
--- setup-edit-server.el
+++
@@ -1,8 +0,0 @@
-(require 'edit-server)
-(setq edit-server-new-frame nil)
-(edit-server-start)
-
-(defadvice edit-server-create-edit-buffer (after edit-server-fullscreen activate)
-  (delete-other-windows))
-
-(provide 'setup-edit-server)
