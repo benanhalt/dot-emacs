5c7c560450733da7883efacf8a5de2da57e1c1c1
Ben Anhalt <ben.anhalt@thread.one>
2023-08-06 14:51:53 -0500

    Fullscreen, dockerfile-mode, restclient.

--- init.el
+++ init.el
@@ -1,2 +1,12 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (load "init-common")
+
+(set-face-attribute 'default nil :height 160)
+(set-frame-parameter nil 'fullscreen 'maximized)
+
+
+(use-package dockerfile-mode
+  :ensure t)
+
+(use-package restclient
+  :ensure t)
