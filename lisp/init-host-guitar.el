6a6f6d491104593d5af43dfbfd7982810c124a09
Ben Anhalt <ben.anhalt@gmail.com>
2023-08-18 16:15:02 -0500

    Add guitar.

---
+++ lisp/init-host-guitar.el
@@ -0,0 +1,10 @@
+
+(set-face-attribute 'default nil :height 160)
+(set-frame-parameter nil 'fullscreen 'maximized)
+
+
+(use-package dockerfile-mode
+  :ensure t)
+
+(use-package restclient
+  :ensure t)
