6e0698fc2294edb9a6bbde1523181a9bc83d01ec
Ben Anhalt <ben.anhalt@gmail.com>
2024-10-24 12:53:55 -0500

    Envrc.

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (load "init-common")
 (load (concat "init-host-" (system-name)))
+
+(use-package envrc
+  :ensure t
+  :hook (after-init . envrc-global-mode))
